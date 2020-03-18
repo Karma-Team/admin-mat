6270b451be01318d104cad3bee6214d2621a7f36
Ghislain Fournier <ghislain.fournier@rez-gif.supelec.fr>
2020-03-18 18:39:29 +0100

    MàJ du cproject pour la crosscompil Docker.

--- src/admin-mat.cpp
+++ src/admin-mat.cpp
@@ -7,25 +7,36 @@
 //============================================================================
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
-//#include <opencv2/aruco.hpp>
+#include <opencv2/aruco.hpp>
 
 #include <iostream>
+#include <thread>
 #include <wiringPi.h>
 
+#include "TCP/TCP_Server.hpp"
+#include "SSV/SSV_ReadWrite.hpp"
+
 using namespace std;
+
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 
+	cout << "Test cmn lib - start TCP server" << endl;
+	string servAddr = "0.0.0.0";
+	TCP::CTcpServer l_CTcpServer(55000, servAddr);
+	//l_CTcpServer = new TCP::CTcpServer(55000, servAddr);
+	l_CTcpServer.initTcpServer();
+
 	// We'll start by loading an image from the drive
-	cv::Mat inputImage = cv::imread((cv::String) "capture.png", cv::IMREAD_COLOR);
+	cout << "Test opencv" << endl;
+	cv::Mat inputImage = cv::imread("capture.png", cv::IMREAD_COLOR);
 
 	// We check that our image has been correctly loaded
 	if (inputImage.empty()) {
 		cout << "Error: the image has been incorrectly loaded."
 				<< endl;
-		return 0;
+		return 1;
 	}
 
 //	// Then we create a window to display our image
@@ -35,16 +46,18 @@ int main() {
 //	cv::imshow("My first OpenCV window", inputImage);
 //	cv::waitKey(0);
 
-//	vector<int> markerIds;
-//	vector<vector<cv::Point2f>> markerCorners, rejectedCandidates;
-//	cv::Ptr<cv::aruco::DetectorParameters> parameters = cv::aruco::DetectorParameters::create();
-//	cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
-//	cv::aruco::detectMarkers(inputImage, dictionary, markerCorners, markerIds, parameters, rejectedCandidates);
-//
-//	cv::Mat outputImage = inputImage.clone();
-//	cv::aruco::drawDetectedMarkers(outputImage, markerCorners, markerIds);
-//
-//	cv::namedWindow("Aurco Detection");
-//	cv::imshow("Aruco Detection", outputImage);
+	vector<int> markerIds;
+	vector<vector<cv::Point2f>> markerCorners, rejectedCandidates;
+	cv::Ptr<cv::aruco::DetectorParameters> parameters = cv::aruco::DetectorParameters::create();
+	cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
+	cv::aruco::detectMarkers(inputImage, dictionary, markerCorners, markerIds, parameters, rejectedCandidates);
+
+	cout << "Nb Marker detected: " << markerIds.size() << endl;
+
+	cv::Mat outputImage = inputImage.clone();
+	cv::aruco::drawDetectedMarkers(outputImage, markerCorners, markerIds);
+
+	cv::imwrite("/home/pi/work/ArucoDetection.png", outputImage);
+	cout << "End" << endl;
 	return 0;
 }
